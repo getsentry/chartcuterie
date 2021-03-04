c794e5ad06825342ba4b1918437596483f1654d7
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-03-04 12:33:48 -0800

    ref: Better code for font importing (#13)

--- src/utils.ts
+++ src/utils.ts
@@ -2,17 +2,16 @@ import * as path from 'path';
 
 import {registerFont} from 'canvas';
 
+function fontFile(name: string) {
+  return path.join(__dirname, '/../fonts/', name);
+}
+
 /**
  * Make our fonts available in node-canvas
  */
 export function registerCanvasFonts() {
-  const lastDir = process.cwd();
-  process.chdir(path.resolve(__dirname, '../fonts'));
-
-  registerFont('./rubik-regular.woff', {family: 'Rubik', weight: 'normal'});
-  registerFont('./rubik-medium.woff', {family: 'Rubik', weight: 'medium'});
-
-  process.chdir(lastDir);
+  registerFont(fontFile('/rubik-regular.woff'), {family: 'Rubik', weight: 'normal'});
+  registerFont(fontFile('/rubik-medium.woff'), {family: 'Rubik', weight: 'medium'});
 }
 
 /**
