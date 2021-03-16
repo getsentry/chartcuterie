f5000379f27f4a76f62fbf5f6081359156248818
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-03-16 16:57:14 -0700

    ref: Bump the json parsing size limit
    It defaults to 100kb

--- src/renderServer.ts
+++ src/renderServer.ts
@@ -14,7 +14,7 @@ import {validateRenderData} from './validate';
 export function renderServer(config: ConfigService) {
   const app = express();
 
-  app.use(express.json());
+  app.use(express.json({limit: '20mb'}));
   app.use(Sentry.Handlers.requestHandler());
 
   app.post('/render', async (req, resp) => {
