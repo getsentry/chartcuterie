7fc1239cb3dd03dd161e2e2a7e5bff1257dc035e
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-08-19 21:19:00 -0400

    make sample rate 0 so it uses the parent sample rate...i think?

--- src/renderServer.ts
+++ src/renderServer.ts
@@ -22,7 +22,7 @@ export function renderServer(config: ConfigService) {
         new Integrations.Express({app}),
         new Sentry.Integrations.Http({ tracing: true }),
       ],
-      tracesSampleRate: 1.0,
+      tracesSampleRate: 0,
     })
   );
 
