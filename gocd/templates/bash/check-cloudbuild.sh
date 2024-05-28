4f2ded42d4d99d2d72f5507ccec93a42ecb6861a
Raj Joshi <raj.joshi@sentry.io>
2024-05-28 15:42:45 -0700

    updated bash scripts to use named arguments

--- gocd/templates/bash/check-cloudbuild.sh
+++ gocd/templates/bash/check-cloudbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /devinfra/scripts/checks/googlecloud/checkcloudbuild.py \
-  "${GO_REVISION_CHARTCUTERIE_REPO}" \
-  sentryio \
-  "us-central1-docker.pkg.dev/sentryio/chartcuterie/image"
+  --sha="${GO_REVISION_CHARTCUTERIE_REPO}" \
+  --project="sentryio" \
+  --image_name="us-central1-docker.pkg.dev/sentryio/chartcuterie/image"
--- gocd/templates/bash/check-github-runs.sh
+++ gocd/templates/bash/check-github-runs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /devinfra/scripts/checks/githubactions/checkruns.py \
-  getsentry/chartcuterie \
-  "${GO_REVISION_CHARTCUTERIE_REPO}" \
-  build
+  --repo="getsentry/chartcuterie" \
+  --sha="${GO_REVISION_CHARTCUTERIE_REPO}" \
+  --check_names="build"
