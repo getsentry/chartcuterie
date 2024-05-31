578d265ecf4acd587030a96734f36508718b38d5
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-05-31 12:50:16 -0700

    fix(gocd): Switching back to positional args (#157)

--- gocd/templates/bash/check-github-runs.sh
+++ gocd/templates/bash/check-github-runs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /devinfra/scripts/checks/githubactions/checkruns.py \
-  --repo="getsentry/chartcuterie" \
-  --sha="${GO_REVISION_CHARTCUTERIE_REPO}" \
-  --check_names="build"
+  "getsentry/chartcuterie" \
+  "${GO_REVISION_CHARTCUTERIE_REPO}" \
+  "build"
