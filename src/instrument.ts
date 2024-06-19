3af0630975533617a9ce2a54011beb708946894a
Jonas <jonas.badalic@sentry.io>
2024-06-19 09:42:29 -0400

    desp: bump sentry to 8.10.0 (#162)

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/node';
+// @ts-expect-error this seems like a regression in the types
 import {nodeProfilingIntegration} from '@sentry/profiling-node';
 
 Sentry.init({
